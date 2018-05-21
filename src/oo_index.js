3f52427eb69f8d22ee525b83ae7081735ceaaad2
github username <ianjpollack@gmail.com>
2018-05-21 15:51:52 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,15 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 });
+
+const field = document.getElementById("new-task-description")
+const list = document.getElementById("tasks")
+
+const submissionForm = document.getElementById("create-task-form")
+submissionForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var newField = document.createElement("li")
+  var content = document.createTextNode(e.target.children[1].value)
+  newField.appendChild(content);
+  list.appendChild(newField)
+})
